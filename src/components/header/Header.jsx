2a74fa60ca0a33873de2759f5b7085e9dfe40fc2
pmh3107 <phanminhhien0701@gmail.com>
2023-12-20 21:35:30 +0700

    Create header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 function Header() {
   return (
-    <>
-      <header>
-        <h1>Đây là header</h1>
-      </header>
-    </>
+    <header className="header">
+      <div className="container">
+        <div className="header__inner">
+          <div className="header__left">
+            <div className="header__left-dot"></div>
+            <p className="header__left-name">
+              <strong className="header__left-name-hl">Phan Minh Hien</strong> /
+              IT intern
+            </p>
+          </div>
+          <div className="header__nav">
+            <ul className="navbar__list">
+              <li className="navbar__item">About me</li>
+              <li className="navbar__item">Project</li>
+              <li className="navbar__item">Contact</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 export default Header;
