c98d8bb85827f621f4bdef7d03312c88b52ae806
pmh3107 <phanminhhien0701@gmail.com>
2023-12-21 21:17:23 +0700

    Completed Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { useState } from "react";
 function Header() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsActive(!isActive);
+  };
   return (
     <header className="header">
       <div className="container">
@@ -12,11 +18,34 @@ function Header() {
             </p>
           </div>
           <div className="header__nav">
-            <ul className="navbar__list">
-              <li className="navbar__item">About me</li>
-              <li className="navbar__item">Project</li>
-              <li className="navbar__item">Contact</li>
-            </ul>
+            <div className="navbar">
+              <div
+                className={`navbar__icon ${isActive ? "active" : ""}`}
+                onClick={toggleNavbar}
+              >
+                <svg
+                  id="hamburger"
+                  className="navbar__icon-svg"
+                  viewBox="0 0 60 40"
+                >
+                  <g
+                    stroke="#0050ff"
+                    strokeWidth="4"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path id="top-line" d="M10,10 L50,10 Z"></path>
+                    <path id="middle-line" d="M10,20 L50,20 Z"></path>
+                    <path id="bottom-line" d="M10,30 L50,30 Z"></path>
+                  </g>
+                </svg>
+              </div>
+              <ul className={`navbar__list ${isActive ? "visible" : ""}`}>
+                <li className="navbar__item active">About me</li>
+                <li className="navbar__item">Project</li>
+                <li className="navbar__item">Contact</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
