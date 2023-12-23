a1309ab73573bbfc3bad71b70254304b042c418d
pmh3107 <phanminhhien0701@gmail.com>
2023-12-23 16:53:18 +0700

    Completed About me

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import AboutMe from "./components/pages/AboutMe";
 import "./scss/main.scss";
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
       <Footer />
     </div>
   );
